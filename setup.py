782596b4d67f3c61193089c5bac589dece328d40
FredThx <fredthx@gmail.com>
2017-11-05 12:16:40 +0100

    for Pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 import FGPIO
  
 setup(
     name='FGPIO',
     version=FGPIO.__version__,
-    packages=find_packages(),
+    packages=['FGPIO'],
     author="FredThx",
     author_email="FredThx@gmail.com",
     description="A package for managing E/S of Raspberry pi or PCduino. Gestion des E/S d'un Raspberry PI ou d'un pcduino",
     long_description=open('README.md').read(),
     #install_requires=["smbus-cffi >= 0.4.1"],
     include_package_data=True,
-    url='',
+    url='https://github.com/FredThx/FGPIO/archive/0.6.6.tar.gz',
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
