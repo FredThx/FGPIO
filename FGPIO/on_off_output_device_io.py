0433c8a3cf867275f2e66db9888f0451417c164c
FredThx <fredthx@gmail.com>
2019-02-10 10:43:07 +0100

    continu blink on on_off_output_device (ie led_io)

--- FGPIO/on_off_output_device_io.py
+++ FGPIO/on_off_output_device_io.py
@@ -4,10 +4,10 @@
 ####################################
 '''
 # A on_off simple output device for rpi_duino_io (Rpi or pcduino)
-# 
+#
 #    exemple : led, buzzer, relay, ...
 #
-#	Do not use this abstract class, but 
+#	Do not use this abstract class, but
 #		- led_io
 #		- buzz_io
 #		- relay_io
@@ -17,7 +17,7 @@
 # Projet : rpiduino_io
 #
 '''
-#################################### 
+####################################
 
 import time
 import functools
@@ -30,7 +30,7 @@ class on_off_output_device_io (device_io):
 	''' on/off output device wired on a rpiduino (pcduino ou Rpi)
 	'''
 	default_blink_time_on = 1
-	
+
 	def __init__(self, pin=None):
 		''' Initialisation
 				pin	:	digital_pin_io
@@ -47,7 +47,7 @@ class on_off_output_device_io (device_io):
 		self.thread = None
 		thread_end_time = None
 		logging.info("%s is created on pin %s." % (self, self.pin))
-	
+
 	def none_none(fonction):
 		'''Décorateur : si pin==None : la fonction ne s'applique pas
 		'''
@@ -58,7 +58,7 @@ class on_off_output_device_io (device_io):
 			else:
 				return fonction(self, *args, **kwargs)
 		return none_none_fonction
-			
+
 	@none_none
 	def set(self, etat):
 		''' change the state of the device
@@ -68,34 +68,34 @@ class on_off_output_device_io (device_io):
 			self.pin.set(HIGH)
 		else:
 			self.pin.set(LOW)
-	
+
 	@none_none
 	def get(self):
 		''' get the device state
 		'''
 		return self.pin.get()
-	
+
 	@none_none
 	def on(self):
 		''' Set on the device
 		'''
 		self.set(True)
-	
+
 	@none_none
 	def off(self):
 		''' Set off the device
 		'''
 		self.set(False)
-	
+
 	@none_none
 	def invert(self):
 		''' Invert the device state
 		'''
 		self.pin.invert()
-	
+
 	@none_none
 	def blink(self, time_on = None, time_off = None, time_end = None):
-		''' Create a thread for blinking the device
+		''' Create (or continu) a thread for blinking the device
 			- time_on	:	duration led is on (second)
 			- time_off	:	duration led is off (second)
 			- time_end	:	stop the thread after time_end seconds
@@ -108,13 +108,14 @@ class on_off_output_device_io (device_io):
 			self.th_time_off = time_on
 		else:
 			self.th_time_off = time_off
+		if time_end==None:
+			self.thread_end_time = None
+		else:
+			self.thread_end_time = time.time() + time_end
 		if self.thread == None:
 			self.thread = f_thread(self._blink)
-			if time_end==None:
-				self.thread_end_time = None
-			else:
-				self.thread_end_time = time.time() + time_end
 			self.thread.start()
+			
 
 	def _blink(self):
 		self.on()
@@ -123,8 +124,8 @@ class on_off_output_device_io (device_io):
 		time.sleep(self.th_time_off)
 		if self.thread_end_time!=None and time.time() > self.thread_end_time:
 			self.stop()
-	
-	
+
+
 	@none_none
 	def stop(self):
 		''' Stop blinking the device
@@ -133,4 +134,3 @@ class on_off_output_device_io (device_io):
 			self.thread.stop()
 			self.thread = None
 		self.off()
-	
